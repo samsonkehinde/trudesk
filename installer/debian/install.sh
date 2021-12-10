bd6aea26cc30a1a463bdf393e8768f7cccc37e73
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2021-12-10 16:41:00 +0100

    Update install.sh

--- installer/debian/install.sh
+++ installer/debian/install.sh
@@ -123,7 +123,6 @@ case $response in
 	echo -e "${YELLOW}Installing Elasticsearch${NC}"
 	wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 	echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
-	apt-get update;
 	apt-get install -y default-jdk;
 	apt-get install -y apt-transport-https elasticsearch kibana;
 	echo "network.host: [_local_]" >> /etc/elasticsearch/elasticsearch.yml
@@ -136,7 +135,6 @@ read -r -p "Do you want to install MongoDB? [y/N]: " response </dev/tty
 case $response in
 [yY]*)
 	echo -e "${YELLOW}Installing MongoDB 4.0${NC}"
-	apt-get install gnupg
 	wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 	echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 	apt-get update
