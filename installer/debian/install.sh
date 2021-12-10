4da513b97662060d158d45c909ec7fcf5035e2fb
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2021-12-10 16:19:01 +0100

    Update install.sh

--- installer/debian/install.sh
+++ installer/debian/install.sh
@@ -124,7 +124,7 @@ case $response in
 	wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 	echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 	apt-get update;
-	apt-get install -y openjdk-8-jre;
+	apt-get install -y default-jdk;
 	apt-get install -y apt-transport-https elasticsearch kibana;
 	echo "network.host: [_local_]" >> /etc/elasticsearch/elasticsearch.yml
 	systemctl enable elasticsearch
